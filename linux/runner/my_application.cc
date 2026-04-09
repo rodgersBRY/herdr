b01de90925d5b17cd109c9169d1e799acd40a8dc
Mawira <brian.mawi@yahoo.com>
2026-04-09 17:12:49 +0300

    rename app

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "cattle_manager");
+    gtk_header_bar_set_title(header_bar, "Herdr");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "cattle_manager");
+    gtk_window_set_title(window, "Herdr");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
